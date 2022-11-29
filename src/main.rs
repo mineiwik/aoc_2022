02a5f2a6293b66d730b0f3b0e89971bfa79384ca
mineiwik <29404520+mineiwik@users.noreply.github.com>
2022-11-29 17:26:49 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -20,160 +20,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .spawn(NodeBundle {
             style: Style {
                 size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                justify_content: JustifyContent::SpaceBetween,
+                flex_direction: FlexDirection::Column,
+                align_items: AlignItems:: Center,
                 ..default()
             },
             ..default()
         })
         .with_children(|parent| {
-            // left vertical fill (border)
             parent
                 .spawn(NodeBundle {
                     style: Style {
-                        size: Size::new(Val::Px(200.0), Val::Percent(100.0)),
-                        border: UiRect::all(Val::Px(2.0)),
-                        ..default()
-                    },
-                    background_color: Color::rgb(0.65, 0.65, 0.65).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    // left vertical fill (content)
-                    parent
-                        .spawn(NodeBundle {
-                            style: Style {
-                                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                                ..default()
-                            },
-                            background_color: Color::rgb(0.15, 0.15, 0.15).into(),
-                            ..default()
-                        })
-                        .with_children(|parent| {
-                            // text
-                            parent.spawn(
-                                TextBundle::from_section(
-                                    "Text Example",
-                                    TextStyle {
-                                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                        font_size: 30.0,
-                                        color: Color::WHITE,
-                                    },
-                                )
-                                .with_style(Style {
-                                    margin: UiRect::all(Val::Px(5.0)),
-                                    ..default()
-                                }),
-                            );
-                        });
-                });
-            // right vertical fill
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        flex_direction: FlexDirection::Column,
+                        size: Size::new(Val::Percent(100.0), Val::Percent(10.0)),
                         justify_content: JustifyContent::Center,
-                        size: Size::new(Val::Px(200.0), Val::Percent(100.0)),
-                        ..default()
-                    },
-                    background_color: Color::rgb(0.15, 0.15, 0.15).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    // Title
-                    parent.spawn(
-                        TextBundle::from_section(
-                            "Scrolling list",
-                            TextStyle {
-                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                font_size: 25.,
-                                color: Color::WHITE,
-                            },
-                        )
-                        .with_style(Style {
-                            size: Size::new(Val::Undefined, Val::Px(25.)),
-                            margin: UiRect {
-                                left: Val::Auto,
-                                right: Val::Auto,
-                                ..default()
-                            },
-                            ..default()
-                        }),
-                    );
-                });
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        size: Size::new(Val::Px(200.0), Val::Px(200.0)),
-                        position_type: PositionType::Absolute,
-                        position: UiRect {
-                            left: Val::Px(210.0),
-                            bottom: Val::Px(10.0),
-                            ..default()
-                        },
-                        border: UiRect::all(Val::Px(20.0)),
-                        ..default()
-                    },
-                    background_color: Color::rgb(0.4, 0.4, 1.0).into(),
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(NodeBundle {
-                        style: Style {
-                            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                            ..default()
-                        },
-                        background_color: Color::rgb(0.8, 0.8, 1.0).into(),
-                        ..default()
-                    });
-                });
-            // render order test: reddest in the back, whitest in the front (flex center)
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                        position_type: PositionType::Absolute,
                         align_items: AlignItems::Center,
-                        justify_content: JustifyContent::Center,
-                        ..default()
-                    },
-                    ..default()
-                })
-                .with_children(|parent| {
-                    for i in 0..25 {
-                        parent
-                            .spawn(NodeBundle {
-                                style: Style {
-                                    size: Size::new(Val::Px(100.0), Val::Px(100.0)),
-                                    ..default()
-                                },
-                                background_color: Color::rgb(1.0, 0.0, 0.0).into(),
-                                ..default()
-                            })
-                            .with_children(|parent| {
-                                parent.spawn(
-                                    TextBundle::from_section(
-                                        format!("{}", i),
-                                        TextStyle {
-                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                            font_size: 30.0,
-                                            color: Color::WHITE,
-                                        },
-                                    )
-                                    .with_style(Style {
-                                        margin: UiRect::all(Val::Px(5.0)),
-                                        ..default()
-                                    }),
-                                );
-                            });
-                    }
-                });
-            parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                        position_type: PositionType::Absolute,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::FlexStart,
                         ..default()
                     },
                     ..default()
@@ -193,6 +52,61 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                             ..default()
                         }),
                     );
+                });
+            parent
+                .spawn(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Percent(90.0), Val::Percent(90.0)),
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::SpaceEvenly,
+                        flex_wrap: FlexWrap::Wrap,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|parent| {
+                    for i in 0..25 {
+                        parent
+                            .spawn(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Percent(15.0), Val::Px(100.0)),
+                                    align_items: AlignItems::Center,
+                                    justify_content: JustifyContent::Center,
+                                    ..default()
+                                },
+                                background_color: Color::rgb(0.1882, 0.2706, 0.4039).into(),
+                                ..default()
+                            })
+                            .with_children(|parent| {
+                                parent.spawn(
+                                    TextBundle::from_section(
+                                        format!("{}", i + 1),
+                                        TextStyle {
+                                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                            font_size: 30.0,
+                                            color: Color::WHITE,
+                                        },
+                                    )
+                                    .with_style(Style {
+                                        position_type: PositionType::Absolute,
+                                        position: UiRect { left: Val::Px(0.0), right: Val::Auto, top: Val::Px(0.0), bottom: Val::Auto },
+                                        margin: UiRect::all(Val::Px(5.0)),
+                                        ..default()
+                                    }),
+                                );
+                                parent.spawn(
+                                    ImageBundle {
+                                        style: Style {
+                                            size: Size::new(Val::Px(75.0), Val::Px(75.0)),
+                                            max_size: Size::new(Val::Percent(75.0), Val::Auto),
+                                            ..default()
+                                        },
+                                        image: asset_server.load("img/snowflake.png").into(),
+                                        ..default()
+                                    }
+                                );
+                            });
+                    }
                 });
         });
 }
